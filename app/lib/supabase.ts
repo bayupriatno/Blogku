ff47afd2db45bdc648f0a0d5302f5e45497c8763
bayupriatno <80336658+bayupriatno@users.noreply.github.com>
2025-07-02 01:08:57 +0700

    Delete app/lib directory

--- app/lib/supabase.ts
+++
@@ -1,9 +0,0 @@
-import { createBrowserClient } from '@supabase/ssr';
-
-// Function to create a Supabase client for client-side operations
-export function createClient() {
-  return createBrowserClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-  );
-}
